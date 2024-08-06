bda00b3db729af91a1bdc4ce3430ee2ae6a39732
Muhamed Jonuzovic <155450175+jonuzovic-muhamed@users.noreply.github.com>
2024-08-06 19:40:47 +0200

    Add solution to JavaScript function exercises
    Added the solution to the four exercises in the JavaScript Function Chapter of the foundation course.

---
+++ js-exercises/function-exercises.js
@@ -0,0 +1,15 @@
+function add7 (someNumber) {
+    return someNumber + 7;
+}
+
+function multiply (firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function capitalize (inputString) {
+   return inputString.charAt(0).toUpperCase() + inputString.substring(1, inputString.length).toLowerCase();
+}
+
+function lastLetter (inputString) {
+    return inputString.charAt(inputString.length - 1);
+}
